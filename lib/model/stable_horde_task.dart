0f2859516c47c7c20bf43ec9b607e1bd8195ecb5
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-11-26 21:45:22 -0800

    wip

---
+++ lib/model/stable_horde_task.dart
@@ -0,0 +1,13 @@
+import 'package:isar/isar.dart';
+
+
+part 'stable_horde_task.g.dart';
+
+@collection
+class StableHordeTask {
+  Id id = Isar.autoIncrement; // you can also use id = null to auto increment
+
+  final String taskId;
+
+  StableHordeTask(this.taskId);
+}
