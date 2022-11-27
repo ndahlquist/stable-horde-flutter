d76e5220727849d89f1acbd517f56c425c972560
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-11-26 22:17:04 -0800

    wip

--- lib/pages/home/discover_tab.dart
+++ lib/pages/home/discover_tab.dart
@@ -2,6 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:zoomscroller/blocs/world_sync_bloc.dart';
+import 'package:zoomscroller/main.dart';
+import 'package:zoomscroller/model/stable_horde_task.dart';
 import 'package:zoomscroller/model/world.dart';
 
 class DiscoverTab extends StatefulWidget {
@@ -11,18 +13,16 @@ class DiscoverTab extends StatefulWidget {
 
 class _DiscoverTabState extends State<DiscoverTab>
     with AutomaticKeepAliveClientMixin {
-  final _worldsStream =
-      worldSyncBloc.getWorldsForDiscover().asBroadcastStream();
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return StreamBuilder<List<World>>(
-      stream: _worldsStream,
+    return StreamBuilder(
+      stream: isar.stableHordeTasks.watchLazy(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
+        /*if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
-        }
+        }*/
 
         if (snapshot.hasError) {
           print(snapshot.error);
@@ -33,27 +33,16 @@ class _DiscoverTabState extends State<DiscoverTab>
             stackTrace: snapshot.stackTrace,
           );
         }
-
-        var data = snapshot.data;
-        if (data == null) {
-          return const Center(
-            child: Text(
-              "Error loading; please try again later.",
-              style: TextStyle(color: Colors.white),
-            ),
-          );
-        }
-
-        data = data.where((element) => element.imageUrl != null).toList();
-
-        return GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            childAspectRatio: 1.0,
-          ),
-          itemCount: data.length,
+        final length = isar.stableHordeTasks.getSizeSync();
+        return ListView.builder(
+          itemCount: length,
           itemBuilder: (context, index) {
-            return _DiscoverTile(world: data![index]);
+            final task = isar.stableHordeTasks.getSync(index);
+            if (task == null) return SizedBox.shrink();
+
+            return ListTile(
+              title: Text(task!.taskId),
+            );
           },
         );
       },
