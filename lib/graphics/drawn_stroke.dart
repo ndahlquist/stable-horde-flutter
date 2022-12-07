36643c732210c9d295b890fc3e9a5a97cb0155bd
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-07 10:43:37 -0800

    wip

--- lib/graphics/drawn_stroke.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DrawnStroke {
-  final List<Offset> path;
-  final Color color;
-  final double width;
-
-  DrawnStroke(this.path, this.color, this.width);
-}
