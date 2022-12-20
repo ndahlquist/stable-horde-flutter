6eea45fc80b63fe36eea89809b66f60b91ed6ac7
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-20 09:54:05 -0800

    wip

--- lib/pages/fullscreen_view_page.dart
+++ lib/pages/fullscreen_view_page.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:stable_horde_flutter/blocs/stable_horde_bloc.dart';
+import 'package:stable_horde_flutter/model/stable_horde_task.dart';
 
 class FullScreenViewPage extends StatelessWidget {
-
   const FullScreenViewPage({super.key});
 
   @override
@@ -10,11 +13,41 @@ class FullScreenViewPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFF230D49),
       appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+      body: StreamBuilder<List<StableHordeTask>>(
+        stream: stableHordeBloc.getTasksStream(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
+          if (snapshot.hasError) {
+            print(snapshot.error);
+            print(snapshot.stackTrace);
+
+            Sentry.captureException(
+              snapshot.error,
+              stackTrace: snapshot.stackTrace,
+            );
+          }
+          var tasks = snapshot.data ?? [];
+
+          tasks = tasks.reversed.toList();
+          return PageView.builder(
+            itemCount: tasks.length,
+            itemBuilder: (context, index) {
+              final task = tasks[index];
+              if (task.imagePath != null)
+                return AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.file(
+                    File(task.imagePath!),
+                  ),
+                );
+              return Placeholder();
+            },
+          );
+        },
       ),
     );
   }
-
 }
