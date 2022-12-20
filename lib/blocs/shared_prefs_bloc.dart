a28846eb1ae41e71d19eaa53e557e298f1314371
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-20 09:16:10 -0800

    wip

---
+++ lib/blocs/shared_prefs_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// Provides access to shared preferences.
+class _SharedPrefsBloc {
+  static const _promptKey = 'PROMPT_KEY';
+  static const _apiKeyKey = 'API_KEY_KEY';
+
+  Future<String> getLastPrompt() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_promptKey) ?? "";
+  }
+
+  Future<void> setPrompt(String prompt) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_promptKey, prompt);
+  }
+
+  Future<String> getApiKey() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_apiKeyKey) ?? "";
+  }
+
+  Future<void> setApiKey(String apiKey) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_apiKeyKey, apiKey);
+  }
+}
+
+final sharedPrefsBloc = _SharedPrefsBloc();
