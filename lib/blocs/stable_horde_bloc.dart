47133f8fce054e9950ed9e0044a76b37ba09d5ac
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-21 09:00:20 -0800

    Implement negative prompt. (#21)

--- lib/blocs/stable_horde_bloc.dart
+++ lib/blocs/stable_horde_bloc.dart
@@ -10,7 +10,10 @@ import 'package:stable_horde_flutter/main.dart';
 import 'package:stable_horde_flutter/model/stable_horde_task.dart';
 
 class _StableHordeBloc {
-  Future requestDiffusion(String prompt) async {
+  Future requestDiffusion() async {
+    final prompt = await sharedPrefsBloc.getPrompt();
+    final negativePrompt = await sharedPrefsBloc.getNegativePrompt();
+
     // Add new task to db.
     final dbId = await isar.writeTxn(() async {
       return isar.stableHordeTasks.put(StableHordeTask(prompt));
@@ -29,7 +32,7 @@ class _StableHordeBloc {
     };
 
     final json = {
-      'prompt': prompt,
+      'prompt': prompt + " ### " + negativePrompt,
       'params': {
         'steps': 30,
         'n': 1,
--- lib/pages/home/dream_tab.dart
+++ lib/pages/home/dream_tab.dart
@@ -1,8 +1,8 @@
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:stable_horde_flutter/blocs/shared_prefs_bloc.dart';
 import 'package:stable_horde_flutter/blocs/stable_horde_bloc.dart';
 import 'package:stable_horde_flutter/pages/home_page.dart';
-import 'package:stable_horde_flutter/pages/prompt_edit_page.dart';
 
 class DreamTab extends StatefulWidget {
   const DreamTab({super.key});
@@ -12,81 +12,100 @@ class DreamTab extends StatefulWidget {
 }
 
 class _DreamTabState extends State<DreamTab> {
-  String _prompt = "";
-
-  @override
-  void initState() {
-    super.initState();
-    sharedPrefsBloc.getLastPrompt().then((value) {
-      setState(() {
-        _prompt = value;
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(height: 16),
-          _promptWidget(),
-          ElevatedButton(
-            onPressed: _attemptToGenerate,
-            child: const Text("Generate"),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _promptWidget() {
-    String promptToShow = _prompt.trim();
-    if (promptToShow.isEmpty) {
-      promptToShow = "Tap here to describe what we're painting today.";
-    }
-
-    return GestureDetector(
-      onTap: () async {
-        final newPrompt = await Navigator.of(context).push<String>(
-          MaterialPageRoute(
-            builder: (_) => PromptEditPage(initialText: _prompt),
-          ),
-        );
-        if (newPrompt == null) return;
-        sharedPrefsBloc.setPrompt(newPrompt);
-        setState(() {
-          _prompt = newPrompt;
-        });
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: FractionallySizedBox(
-          widthFactor: 1,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(.1),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                promptToShow,
-                maxLines: 3,
-                style: TextStyle(
-                  fontSize: _prompt.isEmpty ? 16 : 10,
-                ),
-                textAlign: TextAlign.center,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 16),
+            _promptWidget(),
+            const SizedBox(height: 16),
+            ExpandablePanel(
+              header: const Text(
+                "Advanced Options",
+                style: TextStyle(fontSize: 24),
+              ),
+              collapsed: const SizedBox.shrink(),
+              expanded: _advancedOptions(),
+              theme: const ExpandableThemeData(
+                iconColor: Colors.white,
               ),
             ),
-          ),
+            const SizedBox(height: 64),
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: ElevatedButton(
+                onPressed: _attemptToGenerate,
+                child: const Text("Generate"),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
+  Widget _advancedOptions() {
+    return Column(
+      children: [
+        FutureBuilder<String>(
+          future: sharedPrefsBloc.getNegativePrompt(),
+          builder: (context, snapshot) {
+            final negativePrompt = snapshot.data ?? "";
+
+            return TextField(
+              controller: TextEditingController(text: negativePrompt),
+              decoration: _inputDecoration('Negative Prompt'),
+              keyboardType: TextInputType.multiline,
+              maxLines: 5,
+              textCapitalization: TextCapitalization.sentences,
+              onChanged: (negativePrompt) {
+                sharedPrefsBloc.setNegativePrompt(negativePrompt);
+              },
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _promptWidget() {
+    return FutureBuilder<String>(
+        future: sharedPrefsBloc.getPrompt(),
+        builder: (context, snapshot) {
+          final prompt = snapshot.data ?? "";
+
+          return TextField(
+            controller: TextEditingController(text: prompt),
+            decoration: _inputDecoration('Prompt'),
+            keyboardType: TextInputType.multiline,
+            maxLines: 5,
+            autofocus: true,
+            textCapitalization: TextCapitalization.sentences,
+            onChanged: (prompt) {
+              sharedPrefsBloc.setPrompt(prompt);
+            },
+          );
+        });
+  }
+
+  InputDecoration _inputDecoration(String label) {
+    return InputDecoration(
+      labelText: label,
+      enabledBorder: const UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.white),
+      ),
+      focusedBorder: const UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.white),
+      ),
+    );
+  }
+
   Future _attemptToGenerate() async {
-    if (_prompt.isEmpty) {
+    final prompt = await sharedPrefsBloc.getPrompt();
+    if (prompt.trim().isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text("Please enter a prompt first."),
@@ -95,7 +114,7 @@ class _DreamTabState extends State<DreamTab> {
       return;
     }
 
-    stableHordeBloc.requestDiffusion(_prompt);
+    stableHordeBloc.requestDiffusion();
     homeController.animateToPage(1);
   }
 }
