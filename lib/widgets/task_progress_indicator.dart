74905ba0c810df098a0067ef999cbad81bb8691a
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-22 11:00:57 -0800

    wip

--- lib/widgets/task_progress_indicator.dart
+++ lib/widgets/task_progress_indicator.dart
@@ -1,42 +1,72 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:stable_horde_flutter/model/stable_horde_task.dart';
 import 'package:stable_horde_flutter/widgets/timed_progress_indicator.dart';
 
-class TaskProgressIndicator extends StatelessWidget {
+class TaskProgressIndicator extends StatefulWidget {
   final StableHordeTask task;
   final bool showText;
 
   const TaskProgressIndicator(this.task, {super.key, required this.showText});
 
   @override
+  State<TaskProgressIndicator> createState() => _TaskProgressIndicatorState();
+}
+
+class _TaskProgressIndicatorState extends State<TaskProgressIndicator> {
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Set state every second to update the progress indicator.
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      setState(() {});
+      if (widget.task.isComplete()) {
+        _timer.cancel();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     // Once the task is complete, hide the progress indicator.
-    if (task.isComplete()) {
+    if (widget.task.isComplete()) {
       return const SizedBox.shrink();
     }
 
     final Widget progressIndicator;
-    if (task.estimatedCompletionTime == null) {
+    if (widget.task.estimatedCompletionTime == null) {
       progressIndicator = const Center(child: CircularProgressIndicator());
     } else {
       progressIndicator = Center(
         child: TimedProgressIndicator(
-          startTime: task.firstShowProgressIndicatorTime!,
-          completionTime: task.estimatedCompletionTime!,
+          startTime: widget.task.firstShowProgressIndicatorTime!,
+          completionTime: widget.task.estimatedCompletionTime!,
         ),
       );
     }
 
-    if (!showText) {
+    if (!widget.showText) {
       return progressIndicator;
     }
 
     final String loadingMessage;
-    if (task.estimatedCompletionTime == null) {
+    if (widget.task.estimatedCompletionTime == null) {
       loadingMessage = 'Loading...';
     } else {
-      loadingMessage =
-          'Estimated completion time: ${task.estimatedCompletionTime!.difference(DateTime.now()).inSeconds} seconds';
+      final difference = widget.task.estimatedCompletionTime!.difference(
+        DateTime.now(),
+      );
+      loadingMessage = 'ETA: ${difference.inSeconds} s';
     }
 
     return Stack(
