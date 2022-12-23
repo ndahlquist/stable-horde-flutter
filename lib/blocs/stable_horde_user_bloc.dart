49fecc69670a00cda89f62573c2d806cd884bdb5
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-23 09:36:00 -0800

    Validate API key when signing in, and show user stats. (#38)

---
+++ lib/blocs/stable_horde_user_bloc.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:stable_horde_flutter/blocs/shared_prefs_bloc.dart';
+import 'package:stable_horde_flutter/model/stable_horde_user.dart';
+
+import 'package:http/http.dart' as http;
+
+class _StableHordeUserBloc {
+  Future<StableHordeUser?> lookupUser(String? apiKey) async {
+    apiKey ??= await sharedPrefsBloc.getApiKey();
+
+    if (apiKey == null) {
+      return null;
+    }
+
+    final headers = {
+      'Accept': '* / *',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Connection': 'keep-alive',
+      "Content-Type": "application/json",
+      "apikey": apiKey,
+    };
+
+    final response = await http.get(
+      Uri.parse(
+        'https://stablehorde.net/api/v2/find_user',
+      ),
+      headers: headers,
+    );
+
+    if (response.statusCode == 404) {
+      sharedPrefsBloc.setApiKey(null);
+      return null;
+    }
+
+    if (response.statusCode != 200) {
+      final exception = Exception(
+        'Failed to get user: '
+        '${response.statusCode} ${response.body}',
+      );
+      print(exception);
+      Sentry.captureException(exception, stackTrace: StackTrace.current);
+      return null;
+    }
+
+    final jsonResponse = jsonDecode(response.body);
+    print(jsonResponse);
+
+    return StableHordeUser(
+      jsonResponse['username'],
+      (jsonResponse['kudos'] as num).toInt(),
+      jsonResponse['usage']['requests'],
+      jsonResponse['contributions']['fulfillments'],
+    );
+  }
+}
+
+final stableHordeUserBloc = _StableHordeUserBloc();
---
+++ lib/widgets/user_widget.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:stable_horde_flutter/blocs/shared_prefs_bloc.dart';
+import 'package:stable_horde_flutter/blocs/stable_horde_user_bloc.dart';
+import 'package:stable_horde_flutter/dialogs/login_dialog.dart';
+import 'package:stable_horde_flutter/model/stable_horde_user.dart';
+import 'package:stable_horde_flutter/utils/legal_links.dart';
+
+class UserWidget extends StatefulWidget {
+  const UserWidget({super.key});
+
+  @override
+  State<UserWidget> createState() => _UserWidgetState();
+}
+
+class _UserWidgetState extends State<UserWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<StableHordeUser?>(
+      future: stableHordeUserBloc.lookupUser(null),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print(snapshot.error);
+          print(snapshot.stackTrace);
+          Sentry.captureException(
+            snapshot.error,
+            stackTrace: snapshot.stackTrace,
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Padding(
+            padding: EdgeInsets.only(left: 12),
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        var user = snapshot.data;
+        if (user == null) {
+          return Padding(
+            padding: const EdgeInsets.only(
+              left: 12,
+              right: 12,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "You are currently anonymous. For faster image generation, login to a Stable Horde account.",
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () async {
+                    await showDialog(
+                      context: context,
+                      builder: (context) {
+                        return const LoginDialog();
+                      },
+                    );
+
+                    // Refresh user UI.
+                    setState(() {});
+                  },
+                  child: const Text('Login'),
+                ),
+              ],
+            ),
+          );
+        }
+
+        return Padding(
+          padding: const EdgeInsets.only(
+            left: 12,
+            right: 12,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _loggedInUserWidget(user),
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    await sharedPrefsBloc.setApiKey(null);
+                    setState(() {});
+                  },
+                  child: const Text('Logout'),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _loggedInUserWidget(StableHordeUser user) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFF333738),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(4),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(.3),
+            blurRadius: 4,
+            offset: const Offset(0, 4),
+          ),
+          BoxShadow(
+            color: Colors.black.withOpacity(.1),
+            blurRadius: 2,
+            offset: const Offset(0, -1),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              user.username,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 16),
+            GestureDetector(
+              onTap: () {
+                launchUrlInExternalApp(
+                  'https://dbzer0.com/blog/the-kudos-based-economy-for-the-koboldai-horde/',
+                );
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${NumberFormat.decimalPattern().format(user.kudos)} kudos",
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const Text(
+                    "Kudos represent your contributions to the Stable Horde community. Kudos increase your request priority, speeding up your image generations. Kudos are consumed when generating images, and created by running an worker.",
+                  ),
+                  const SizedBox(height: 4),
+                  const Text(
+                    "LEARN MORE",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              "${NumberFormat.decimalPattern().format(user.numRequested)} images generated",
+              style: const TextStyle(fontSize: 18),
+            ),
+            Text(
+              "${NumberFormat.decimalPattern().format(user.numInferences)} images contributed",
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
