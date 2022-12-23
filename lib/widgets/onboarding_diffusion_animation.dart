e687b9d6fd7ae1b18cd9ca19249fcc02789a8f71
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-23 12:09:09 -0800

    wip

--- lib/widgets/onboarding_diffusion_animation.dart
+++ lib/widgets/onboarding_diffusion_animation.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -7,20 +6,50 @@ class OnboardingDiffusionAnimation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 1,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Spacer(),
-          SvgPicture.asset(
-            'assets/vector_drawables/box.svg',
-            width: 64, height: 64,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white12,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: FractionallySizedBox(
+          widthFactor: 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Spacer(
+                flex: 3,
+              ),
+              const Text(
+                '"A Renaissance painting of a robotic cat. Highly detailed."',
+                style: TextStyle(fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+              const Spacer(),
+              const Icon(
+                Icons.arrow_downward_rounded,
+                size: 32,
+              ),
+              const Spacer(),
+              SvgPicture.asset(
+                'assets/vector_drawables/box.svg',
+                width: 64,
+                height: 64,
+              ),
+              const Spacer(),
+              const Icon(
+                Icons.arrow_downward_rounded,
+                size: 32,
+              ),
+              const Spacer(),
+              const Spacer(
+                flex: 3,
+              ),
+            ],
           ),
-          Spacer(),
-        ],
+        ),
       ),
     );
   }
-
 }
