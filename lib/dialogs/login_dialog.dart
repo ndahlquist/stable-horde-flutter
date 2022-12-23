70bc25079096c0406ed5cde1d7ec123505aa8136
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-23 09:17:49 -0800

    wip

--- lib/dialogs/login_dialog.dart
+++ lib/dialogs/login_dialog.dart
@@ -6,18 +6,42 @@ class LoginDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text('API Key'),
-      content: TextField(
-        onChanged: (value) async {
-          await sharedPrefsBloc.setApiKey(value);
-        },
+     // title: const Text('API Key'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          RichText(
+            text: TextSpan(
+              text: 'Enter your Stable Horde API key to login. You can get one at\n',
+              style: DefaultTextStyle.of(context).style,
+              children: const <TextSpan>[
+                TextSpan(text: 'https://stablehorde.net/register', style: TextStyle(fontWeight: FontWeight.bold)),
+                TextSpan(text: '.'),
+              ],
+            ),
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              labelText: 'API Key',
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+            ),
+            onChanged: (value) async {
+              await sharedPrefsBloc.setApiKey(value);
+            },
+          ),
+        ],
       ),
       actions: [
         TextButton(
           onPressed: () {
             Navigator.of(context).pop();
           },
-          child: const Text('Continue'),
+          child: const Text('Validate'),
         ),
       ],
     );
