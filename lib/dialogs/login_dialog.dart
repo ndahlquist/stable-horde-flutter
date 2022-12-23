29e9927fd439cb665477a4df8eda946e4eb677b1
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-23 09:19:05 -0800

    wip

--- lib/dialogs/login_dialog.dart
+++ lib/dialogs/login_dialog.dart
@@ -1,23 +1,30 @@
-
 import 'package:flutter/material.dart';
 import 'package:stable_horde_flutter/blocs/shared_prefs_bloc.dart';
+import 'package:stable_horde_flutter/utils/legal_links.dart';
 
 class LoginDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-     // title: const Text('API Key'),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          RichText(
-            text: TextSpan(
-              text: 'Enter your Stable Horde API key to login. You can get one at\n',
-              style: DefaultTextStyle.of(context).style,
-              children: const <TextSpan>[
-                TextSpan(text: 'https://stablehorde.net/register', style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(text: '.'),
-              ],
+          GestureDetector(
+            onTap: () {
+              launchUrlInExternalApp('https://stablehorde.net/register');
+            },
+            child: RichText(
+              text: TextSpan(
+                text:
+                    'Enter your Stable Horde API key to login. You can get one at\n',
+                style: DefaultTextStyle.of(context).style,
+                children: const <TextSpan>[
+                  TextSpan(
+                      text: 'https://stablehorde.net/register',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(text: '.'),
+                ],
+              ),
             ),
           ),
           TextField(
@@ -46,5 +53,4 @@ class LoginDialog extends StatelessWidget {
       ],
     );
   }
-
 }
