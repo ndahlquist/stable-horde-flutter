efc80424afab0986001e43163f4a4f5401522c91
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-23 08:49:45 -0800

    wip

--- lib/widgets/user_widget.dart
+++ lib/widgets/user_widget.dart
@@ -33,51 +33,74 @@ class UserWidget extends StatelessWidget {
             right: 12,
             bottom: 64,
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                data.username,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xFF383333),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(4),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(.3),
+                  blurRadius: 4,
+                  offset: const Offset(0, 4),
                 ),
-              ),
-              const SizedBox(height: 16),
-              GestureDetector(
-                onTap: () {
-                  launchUrlInExternalApp(
-                    'https://dbzer0.com/blog/the-kudos-based-economy-for-the-koboldai-horde/',
-                  );
-                },
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "${NumberFormat.decimalPattern().format(data.kudos)} kudos",
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                    const Text(
-                      "Kudos represent your contributions to the Stable Horde community. Kudos increase your request priority, speeding up your image generations. Kudos are consumed when generating images, and created by running an worker.",
-                    ),
-                    const SizedBox(height: 4),
-                    const Text(
-                      "LEARN MORE",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
+                BoxShadow(
+                  color: Colors.black.withOpacity(.1),
+                  blurRadius: 2,
+                  offset: const Offset(0, -1),
                 ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    data.username,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  GestureDetector(
+                    onTap: () {
+                      launchUrlInExternalApp(
+                        'https://dbzer0.com/blog/the-kudos-based-economy-for-the-koboldai-horde/',
+                      );
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${NumberFormat.decimalPattern().format(data.kudos)} kudos",
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                        const Text(
+                          "Kudos represent your contributions to the Stable Horde community. Kudos increase your request priority, speeding up your image generations. Kudos are consumed when generating images, and created by running an worker.",
+                        ),
+                        const SizedBox(height: 4),
+                        const Text(
+                          "LEARN MORE",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    "${NumberFormat.decimalPattern().format(data.numRequested)} images generated",
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    "${NumberFormat.decimalPattern().format(data.numInferences)} images contributed",
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ],
               ),
-              const SizedBox(height: 16),
-              Text(
-                "${NumberFormat.decimalPattern().format(data.numRequested)} images generated",
-                style: const TextStyle(fontSize: 18),
-              ),
-              Text(
-                "${NumberFormat.decimalPattern().format(data.numInferences)} images contributed",
-                style: const TextStyle(fontSize: 18),
-              ),
-            ],
+            ),
           ),
         );
       },
