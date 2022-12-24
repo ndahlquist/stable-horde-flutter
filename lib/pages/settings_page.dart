852d2e24bc38a390b21d94d954b31816381ad2db
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-23 16:35:03 -0800

    wip

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:stable_horde_flutter/colors.dart';
 import 'package:stable_horde_flutter/pages/onboarding_page.dart';
 import 'package:stable_horde_flutter/utils/legal_links.dart';
+import 'package:stable_horde_flutter/widgets/glassmorphic_background.dart';
 import 'package:stable_horde_flutter/widgets/user_widget.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -9,97 +9,102 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: stableHordeGrey,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: const Text('Settings'),
-      ),
-      body: SingleChildScrollView(
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const UserWidget(),
-            const Padding(
-              padding: EdgeInsets.only(
-                top: 64.0,
-                left: 16.0,
-                bottom: 16,
-              ),
-              child: Text(
-                'Learn More',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            ListTile(
-              title: const Text('StableHorde.net'),
-              onTap: () {
-                launchUrlInExternalApp('https://stablehorde.net');
-              },
-            ),
-            ListTile(
-              title: const Text('FAQ'),
-              onTap: () {
-                launchUrlInExternalApp(
-                  'https://github.com/db0/AI-Horde/blob/main/FAQ.md',
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Discord'),
-              onTap: () {
-                launchUrlInExternalApp('https://discord.gg/3DxrhksKzn');
-              },
-            ),
-            ListTile(
-              title: const Text('Restart Tutorial'),
-              onTap: () {
-                Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(
-                    builder: (_) => const OnboardingPage(),
+    return Stack(
+      children: [
+        const GlassmorphicBackground(),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            title: const Text('Settings'),
+          ),
+          body: SingleChildScrollView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const UserWidget(),
+                const Padding(
+                  padding: EdgeInsets.only(
+                    top: 64.0,
+                    left: 16.0,
+                    bottom: 16,
                   ),
-                  (route) => false,
-                );
-              },
-            ),
-            const Padding(
-              padding: EdgeInsets.only(
-                top: 64.0,
-                left: 16.0,
-                bottom: 16,
-              ),
-              child: Text(
-                'Legal',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+                  child: Text(
+                    'Learn More',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
                 ),
-                textAlign: TextAlign.left,
-              ),
+                ListTile(
+                  title: const Text('StableHorde.net'),
+                  onTap: () {
+                    launchUrlInExternalApp('https://stablehorde.net');
+                  },
+                ),
+                ListTile(
+                  title: const Text('FAQ'),
+                  onTap: () {
+                    launchUrlInExternalApp(
+                      'https://github.com/db0/AI-Horde/blob/main/FAQ.md',
+                    );
+                  },
+                ),
+                ListTile(
+                  title: const Text('Discord'),
+                  onTap: () {
+                    launchUrlInExternalApp('https://discord.gg/3DxrhksKzn');
+                  },
+                ),
+                ListTile(
+                  title: const Text('Restart Tutorial'),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                        builder: (_) => const OnboardingPage(),
+                      ),
+                      (route) => false,
+                    );
+                  },
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(
+                    top: 64.0,
+                    left: 16.0,
+                    bottom: 16,
+                  ),
+                  child: Text(
+                    'Legal',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                const ListTile(
+                  title: Text('Privacy Policy'),
+                  onTap: launchPrivacyPolicy,
+                ),
+                const ListTile(
+                  title: Text('Terms of Service'),
+                  onTap: launchTermsOfService,
+                ),
+                ListTile(
+                  title: const Text('Third Party Software'),
+                  onTap: () {
+                    showLicensePage(context: context);
+                  },
+                ),
+              ],
             ),
-            const ListTile(
-              title: Text('Privacy Policy'),
-              onTap: launchPrivacyPolicy,
-            ),
-            const ListTile(
-              title: Text('Terms of Service'),
-              onTap: launchTermsOfService,
-            ),
-            ListTile(
-              title: const Text('Third Party Software'),
-              onTap: () {
-                showLicensePage(context: context);
-              },
-            ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
