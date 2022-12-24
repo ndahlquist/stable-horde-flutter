184a5db59a35777490ae53e3bd2efcbb2ed4f111
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-23 16:24:12 -0800

    wip

--- lib/widgets/glassmorphic_background.dart
+++ lib/widgets/glassmorphic_background.dart
@@ -1,14 +1,42 @@
+import 'dart:async';
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class GlassmorphicBackground extends StatelessWidget {
+class GlassmorphicBackground extends StatefulWidget {
+  const GlassmorphicBackground({super.key});
+
+  @override
+  State<GlassmorphicBackground> createState() => _GlassmorphicBackgroundState();
+}
+
+class _GlassmorphicBackgroundState extends State<GlassmorphicBackground> {
+
+  late Timer _timer;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic( const Duration(milliseconds: 32), (timer) {
+      setState(() {});
+    });
+  }
+
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ImageFiltered(
       imageFilter: ImageFilter.blur(
-        sigmaX: 100,
-        sigmaY: 100,
+        sigmaX: 97,
+        sigmaY: 97,
         tileMode: TileMode.clamp,
       ),
       child: Stack(
@@ -26,13 +54,13 @@ class GlassmorphicBackground extends StatelessWidget {
             ),
           ),
           Positioned(
-            top: 100,
-            left: 100,
+            top: 100 + sin(_timer.tick / 198.0) * 50,
+            left: 100 + sin(_timer.tick / 211.0) * 50,
             child: _circle(const Color(0xFF005F7C), 400),
           ),
           Positioned(
-            bottom: 100,
-            right: 100,
+            bottom: 100 + sin(_timer.tick / 100.0) * 50,
+            right: 100 + sin(_timer.tick / 121.0) * 50,
             child: _circle(const Color(0xFFA63802 ), 200),
           ),
         ],
