dc35d24148316f4c2cff17142b80ce8ffc1e75c9
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-12-24 17:02:33 -0800

    wip

--- lib/widgets/glassmorphic_background.dart
+++ lib/widgets/glassmorphic_background.dart
@@ -31,6 +31,8 @@ class _GlassmorphicBackgroundState extends State<GlassmorphicBackground> {
 
   @override
   Widget build(BuildContext context) {
+    final tick = DateTime.now().millisecondsSinceEpoch;
+
     return ClipRect(
       child: Stack(
         children: [
@@ -47,18 +49,18 @@ class _GlassmorphicBackgroundState extends State<GlassmorphicBackground> {
             ),
           ),
           Positioned(
-            bottom: 100 + sin(_timer.tick / 100.0) * 50,
-            right: 100 + sin(_timer.tick / 121.0) * 50,
-            child: _circle(const Color(0xFF812B00), 400),
+            bottom: 100 + sin(tick / 5100.0) * 50,
+            right: 100 + sin(tick / 5121.0) * 50,
+            child: _circle(const Color(0xAA812B00), 400),
           ),
           Positioned(
-            top: 100 + sin(_timer.tick / 205.0) * 50,
-            left: 100 + sin(_timer.tick / 225.0) * 50,
-            child: _circle(const Color(0xFF812B00), 200),
+            top: 100 + sin(tick / 5505.0) * 50,
+            left: 100 + sin(tick / 5825.0) * 50,
+            child: _circle(const Color(0x32814D00), 200),
           ),
           Positioned(
-            top: 100 + sin(_timer.tick / 198.0) * 50,
-            left: 100 + sin(_timer.tick / 211.0) * 50,
+            top: 100 + sin(tick / 4198.0) * 50,
+            left: 100 + sin(tick / 4211.0) * 50,
             child: _circle(const Color(0xFF240050), 800),
           ),
         ],
@@ -67,6 +69,7 @@ class _GlassmorphicBackgroundState extends State<GlassmorphicBackground> {
   }
 
   Widget _circle(Color color, double radius) {
+    final originalOpacity = color.opacity;
     return SizedBox(
       width: radius,
       height: radius,
@@ -80,11 +83,11 @@ class _GlassmorphicBackgroundState extends State<GlassmorphicBackground> {
             // (The goal here is to approximate a Gaussian blur,
             // but without actually performing an expensive blur).
             colors: [
-              color.withOpacity(0.199 * 5),
-              color.withOpacity(0.175 * 5),
-              color.withOpacity(0.122 * 5),
-              color.withOpacity(0.066 * 5),
-              color.withOpacity(0.028 * 5),
+              color.withOpacity(originalOpacity * 0.199 * 5),
+              color.withOpacity(originalOpacity * 0.175 * 5),
+              color.withOpacity(originalOpacity * 0.122 * 5),
+              color.withOpacity(originalOpacity * 0.066 * 5),
+              color.withOpacity(originalOpacity * 0.028 * 5),
               color.withOpacity(0.00),
             ],
             stops: const [
