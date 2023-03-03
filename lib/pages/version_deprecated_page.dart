3a2d1ea6985b12c3a75220d7f280d7505066efc8
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2023-03-03 09:45:33 +0900

    Make VersionDeprecatedPage stateless.

--- lib/pages/version_deprecated_page.dart
+++ lib/pages/version_deprecated_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:stable_horde_flutter/widgets/glassmorphic_background.dart';
 
-class VersionDeprecatedPage extends StatefulWidget {
+class VersionDeprecatedPage extends StatelessWidget {
   const VersionDeprecatedPage({super.key});
 
-  @override
-  State<VersionDeprecatedPage> createState() => _DeprecatedPageState();
-}
-
-class _DeprecatedPageState extends State<VersionDeprecatedPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
